b48f2ad332f994653634e98842198b8e424382f3
ystays <85421593+ystays@users.noreply.github.com>
2023-11-23 15:00:19 +0800

    Worked on backend API, routes, middleware, sample data, database

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 9000;
 
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("API is running");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    let product = products.find((p) => p.name === req.params.id)
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
